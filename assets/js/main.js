71f753c09c7abcdbbc85c9f015fad6befef2cb7f
python-programing <79223364+python-coding-404@users.noreply.github.com>
2022-11-11 17:05:35 +0400

    xhshx

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $('#lol').click(function(){
+        $("#lol").addClass("blue")
+    })
+    $("#l").click(function(){
+        $("#lol").removeClass("blue")
+    })
+  
+  });
