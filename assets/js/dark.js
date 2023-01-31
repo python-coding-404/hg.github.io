cf940077167ce1f08bcff74bd819df2c3bfc8931
python-programing <79223364+python-coding-404@users.noreply.github.com>
2023-01-31 17:45:29 +0400

    xsxs

---
+++ assets/js/dark.js
@@ -0,0 +1,34 @@
+let button_nav = document.getElementById("mode")
+let navbar = document.querySelector(".navbar")
+let body = document.body
+button_nav.addEventListener("click", function(event) {
+    
+    if (body.classList.contains("bg-light")) {
+        body.classList.add("bg-dark");
+        navbar.classList.remove("navbar-light")
+        navbar.classList.remove("bg-light")
+        navbar.classList.add("navbar-dark")
+        navbar.classList.add("bg-dark")
+
+        
+        
+
+
+    } else {
+        navbar.classList.remove("navbar-dark")
+        navbar.classList.remove("bg-dark")
+        body.classList.remove("bg-dark");
+        navbar.classList.add("navbar-light")
+        navbar.classList.add("bg-light")
+        body.classList.add("bg-light");
+
+
+
+
+        
+
+
+    }
+    
+
+})
